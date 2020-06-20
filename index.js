69bed90f56169053a50e5323b713a27b8b83932d
Alex Miller <alexm@getcraft.com>
2020-06-20 10:44:38 +0800

    add date to console log

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const database = require('./database');
 const db = database.db;
 
 const run = async () => {
+	console.log( "Fetching for " + new Date());
 	const data = await fetchDailyData()
 	console.log("scraped")
 	const stored = await storeParsedResults(data)
